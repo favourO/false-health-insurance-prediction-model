68edd655e02945cda1432fc2e5c97b757e1a26cd
Favour Ojiaku <ojiakufavour@gmail.com>
2022-04-22 10:02:59 +0100

    completed project model and data analysis

---
+++ ml_server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+
+// set up our app and middleware
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+// Import router
+const mlRoutes = require('./routes/ml')
+app.use('/api/ml', mlRoutes);
+
+// Start server
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+    console.log(`Server listening on PORT ${port}`)
+})
